a174d6af8060b09837d0102b50b38b529e3bd7e2
Tushar Kaundal <kaundaltushar@gmail.com>
2025-02-11 12:59:42 +0530

    added state of input

--- src/Countries.jsx
+++ src/Countries.jsx
@@ -2,18 +2,20 @@
 import { useEffect, useState } from "react";
 import styles from "./index.module.css";
 
-const CountryCard = ({ name, flag, abbr }) => {
+const CountryCard = ({ common, png }) => {
   return (
     <div className={styles.card}>
-      <img className={styles.cardImage} src={flag} alt={abbr} />
-      <h3>{name}</h3>
+      <img className={styles.cardImage} src={png} alt={common} />
+      <h3>{common}</h3>
     </div>
   );
 };
 
 const Countries = () => {
-  const API_ENDPOINT = "https://xcountries-backend.azurewebsites.net/all";
+  const API_ENDPOINT =
+    "https://countries-search-data-prod-812920491762.asia-south1.run.app/countries";
   const [flagData, setFlagData] = useState([]);
+  const [text, setText] = useState("");
   const fetchData = async () => {
     try {
       const data = await fetch(API_ENDPOINT);
@@ -29,7 +31,11 @@ const Countries = () => {
 
   return (
     <div>
-      <input type="text" />
+      <input
+        type="text"
+        value={text}
+        onChange={(ev) => setText(ev.target.value)}
+      />
       <div className={styles.grid}>
         {flagData.map((data, idx) => (
           <CountryCard key={`country-${idx}`} {...data} />
